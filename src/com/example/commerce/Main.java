464d1d608c42dc92612ff87f63719adf000b13e0
jhsky3118 <doksakim7@naver.com>
2026-01-19 14:22:58 +0900

    feat: 첫 커밋

---
+++ src/com/example/commerce/Main.java
@@ -0,0 +1,15 @@
+package com.example.commerce;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jeongjihun
+ * Date: 26. 1. 19.
+ * Time: 오후 2:04
+ **/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Commerce!");
+    }
+}
