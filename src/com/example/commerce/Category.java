f73a1977d705ccee170735d9860630b3d0329e67
jhsky3118 <doksakim7@naver.com>
2026-01-22 12:45:50 +0900

    refactor: 문제 의도에 맞게(상품 리스트 관리를 Category에게 완전히 맡김) 코드 개선

--- src/com/example/commerce/Category.java
+++ src/com/example/commerce/Category.java
@@ -1,6 +1,7 @@
 package com.example.commerce;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,9 +17,10 @@ public class Category {
     private String name;
     private List<Product> products;
 
-    public Category(String name, List<Product> products) {
+    // !: 리스트의 관리 책임은 Category 클래스이기 때문에 CommerceSystem은 리스트를 몰라도 됨
+    public Category(String name) {
         this.name = name;
-        this.products = products;
+        this.products = new ArrayList<>();
     }
 
     // name 을 반환하는 메서드(게터)
@@ -26,8 +28,23 @@ public class Category {
         return name;
     }
 
-    // 전자제품 카테고리 리스트화
-    public List<Product> makeElecList() {
+    // 전자제품을 추가한 리스트(products)를 불러오는 게터
+    public List<Product> getElecList() {
+        return products;
+    }
+
+    // 의류를 추가한 리스트(products)를 불러오는 게터
+    public List<Product> getClothList() {
+        return products;
+    }
+
+    // 식품을 추가한 리스트(products)를 불러오는 게터
+    public List<Product> getFoodList() {
+        return products;
+    }
+
+    // 전자제품 카테고리 리스트화 // !: 리스트에 추가만 하므로 반환값 없어도 됨
+    public void addElecList() {
         Product product1 = new Product("Galaxy S25", 1_200_000, "최신 안드로이드 스마트폰", 200);
         Product product2 = new Product("iPhone 16", 1_350_000, "Apple의 최신 스마트폰", 150);
         Product product3 = new Product("MacBook Pro", 2_400_000, "M3 칩셋이 탑재된 노트북", 100);
@@ -37,12 +54,10 @@ public class Category {
         this.products.add(product2);
         this.products.add(product3);
         this.products.add(product4);
-
-        return products;
     }
 
     // 의류 카테고리 리스트화
-    public List<Product> makeClothList() {
+    public void addClothList() {
         Product product1 = new Product("티셔츠", 35_000, "평범한 반팔 티셔츠", 200);
         Product product2 = new Product("후드", 150_000, "유행하는 후드", 75);
         Product product3 = new Product("츄리닝", 700_000, "이탈리아 장인이 만든 츄리닝", 10);
@@ -52,12 +67,10 @@ public class Category {
         this.products.add(product2);
         this.products.add(product3);
         this.products.add(product4);
-
-        return products;
     }
 
     // 식품 카테고리 리스트화
-    public List<Product> makeFoodList() {
+    public void addFoodList() {
         Product product1 = new Product("초밥", 15_000, "모듬 초밥", 50);
         Product product2 = new Product("햄버거", 7_500, "불고기 버거", 250);
         Product product3 = new Product("치킨", 25_000, "AI 치킨", 150);
@@ -67,8 +80,6 @@ public class Category {
         this.products.add(product2);
         this.products.add(product3);
         this.products.add(product4);
-
-        return products;
     }
 
 }
--- src/com/example/commerce/CommerceSystem.java
+++ src/com/example/commerce/CommerceSystem.java
@@ -1,8 +1,6 @@
 package com.example.commerce;
 
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -15,19 +13,20 @@ import java.util.Scanner;
 // !: 공부하면서 알게 된 내용
 public class CommerceSystem {
 
-    // 객체 생성
-    List<Product> elecProducts = new ArrayList<>();
-    List<Product> clothProducts = new ArrayList<>();
-    List<Product> foodProducts = new ArrayList<>();
-    Category category1 = new Category("전자제품", elecProducts);
-    Category category2 = new Category("의류", clothProducts);
-    Category category3 = new Category("식품", foodProducts);
+    // !: 문제 요구 사항에서 리스트 관리는 Category가 맡는다고 나와 있어서 코드를 수정해야 할 필요성을 알게됨
+    Category category1 = new Category("전자제품");
+    Category category2 = new Category("의류");
+    Category category3 = new Category("식품");
 
     // 입출력 비즈니스 로직 구현
     public void start() {
-        this.elecProducts = category1.makeElecList();
-        this.clothProducts = category2.makeClothList();
-        this.foodProducts = category3.makeFoodList();
+
+        Scanner sc = new Scanner(System.in);
+
+        // 리스트 생성
+        category1.addElecList();
+        category2.addClothList();
+        category3.addFoodList();
 
         while (true) {
             System.out.println("[ 실시간 커머스 플랫폼 메인 ]");
@@ -36,20 +35,19 @@ public class CommerceSystem {
             System.out.println("3. " + category3.getCategoryName());
             System.out.println("0. 종료 | 프로그램 종료");
 
-            Scanner sc = new Scanner(System.in);
             int inputNumber = sc.nextInt();
 
             // 1번 전자제품 목록의 물품 내역 출력 후 그 중 하나 선택하면 재고까지 출력하는 기능 구현
             if (inputNumber == 1) {
-                for (int i = 0; i < elecProducts.size(); i++) {
-                    System.out.println((i + 1) + ". " + elecProducts.get(i).getName() + " | " + elecProducts.get(i).getPrice() + "원 | " + elecProducts.get(i).getDescription());
+                for (int i = 0; i < category1.getElecList().size(); i++) {
+                    System.out.println((i + 1) + ". " + category1.getElecList().get(i).getName() + " | " + category1.getElecList().get(i).getPrice() + "원 | " + category1.getElecList().get(i).getDescription());
                 }
-                System.out.println("0. 뒤로가기"); // 뒤로가기 추가
+                System.out.println("0. 뒤로가기");
 
                 while (true) {
                     int inputElecNumber = sc.nextInt();
-                    if(inputElecNumber > 0 && inputElecNumber <= elecProducts.size()) {
-                        System.out.println("선택한 상품: " + elecProducts.get(inputElecNumber - 1).getName() + " | " + elecProducts.get(inputElecNumber - 1).getPrice() + "원 | " + elecProducts.get(inputElecNumber - 1).getDescription() + " | 재고: " + elecProducts.get(inputElecNumber - 1).getQuantity() + "개");
+                    if(inputElecNumber > 0 && inputElecNumber <= category1.getElecList().size()) {
+                        System.out.println("선택한 상품: " + category1.getElecList().get(inputElecNumber - 1).getName() + " | " + category1.getElecList().get(inputElecNumber - 1).getPrice() + "원 | " + category1.getElecList().get(inputElecNumber - 1).getDescription() + " | 재고: " + category1.getElecList().get(inputElecNumber - 1).getQuantity() + "개");
                         break;
                     } else if(inputElecNumber == 0) {
                         break;
@@ -60,15 +58,15 @@ public class CommerceSystem {
 
               // 2번 의류 목록의 물품 내역 출력 후 그 중 하나 선택하면 재고까지 출력하는 기능 구현
             } else if (inputNumber == 2) {
-                for (int i = 0; i < clothProducts.size(); i++) {
-                    System.out.println((i + 1) + ". " + clothProducts.get(i).getName() + " | " + clothProducts.get(i).getPrice() + "원 | " + clothProducts.get(i).getDescription());
+                for (int i = 0; i < category2.getClothList().size(); i++) {
+                    System.out.println((i + 1) + ". " + category2.getClothList().get(i).getName() + " | " + category2.getClothList().get(i).getPrice() + "원 | " + category2.getClothList().get(i).getDescription());
                 }
-                System.out.println("0. 뒤로가기"); // 뒤로가기 추가
+                System.out.println("0. 뒤로가기");
 
                 while (true) {
                     int inputElecNumber = sc.nextInt();
-                    if(inputElecNumber > 0 && inputElecNumber <= clothProducts.size()) {
-                        System.out.println("선택한 상품: " + clothProducts.get(inputElecNumber - 1).getName() + " | " + clothProducts.get(inputElecNumber - 1).getPrice() + "원 | " + clothProducts.get(inputElecNumber - 1).getDescription() + " | 재고: " + clothProducts.get(inputElecNumber - 1).getQuantity() + "개");
+                    if(inputElecNumber > 0 && inputElecNumber <= category2.getClothList().size()) {
+                        System.out.println("선택한 상품: " + category2.getClothList().get(inputElecNumber - 1).getName() + " | " + category2.getClothList().get(inputElecNumber - 1).getPrice() + "원 | " + category2.getClothList().get(inputElecNumber - 1).getDescription() + " | 재고: " + category2.getClothList().get(inputElecNumber - 1).getQuantity() + "개");
                         break;
                     } else if(inputElecNumber == 0) {
                         break;
@@ -79,15 +77,15 @@ public class CommerceSystem {
 
               // 3번 식품 목록의 물품 내역 출력 후 그 중 하나 선택하면 재고까지 출력하는 기능 구현
             } else if (inputNumber == 3) {
-                for (int i = 0; i < foodProducts.size(); i++) {
-                    System.out.println((i + 1) + ". " + foodProducts.get(i).getName() + " | " + foodProducts.get(i).getPrice() + "원 | " + foodProducts.get(i).getDescription());
+                for (int i = 0; i < category3.getFoodList().size(); i++) {
+                    System.out.println((i + 1) + ". " + category3.getFoodList().get(i).getName() + " | " + category3.getFoodList().get(i).getPrice() + "원 | " + category3.getFoodList().get(i).getDescription());
                 }
-                System.out.println("0. 뒤로가기"); // 뒤로가기 추가
+                System.out.println("0. 뒤로가기");
 
                 while (true) {
                     int inputElecNumber = sc.nextInt();
-                    if(inputElecNumber > 0 && inputElecNumber <= foodProducts.size()) {
-                        System.out.println("선택한 상품: " + foodProducts.get(inputElecNumber - 1).getName() + " | " + foodProducts.get(inputElecNumber - 1).getPrice() + "원 | " + foodProducts.get(inputElecNumber - 1).getDescription() + " | 재고: " + foodProducts.get(inputElecNumber - 1).getQuantity() + "개");
+                    if(inputElecNumber > 0 && inputElecNumber <= category3.getFoodList().size()) {
+                        System.out.println("선택한 상품: " + category3.getFoodList().get(inputElecNumber - 1).getName() + " | " + category3.getFoodList().get(inputElecNumber - 1).getPrice() + "원 | " + category3.getFoodList().get(inputElecNumber - 1).getDescription() + " | 재고: " + category3.getFoodList().get(inputElecNumber - 1).getQuantity() + "개");
                         break;
                     } else if(inputElecNumber == 0) {
                         break;
